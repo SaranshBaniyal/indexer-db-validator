78c65414d6fdbf2c30354dead915bd23f16da520
Saransh Baniyal <saranshbaniyal14@gmail.com>
2024-05-21 17:08:00 +0530

    individual checks on block_metadata, txn_fact_table, msg_fact_table

---
+++ validator-script.py
@@ -0,0 +1,90 @@
+import logging
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
+logger = logging.getLogger()
+
+class IndexerDatabase:
+    def __init__(self, db_name, user, password, host, port):
+
+        self._PG_DB = db_name
+        self._PG_USER = user
+        self._PG_PW = password
+        self._PG_HOST = host
+        self._PG_PORT = port
+
+        try:
+            self.conn = psycopg2.connect(database=self._PG_DB,
+                                                user=self._PG_USER,
+                                                password=self._PG_PW,
+                                                host=self._PG_HOST,
+                                                port=self._PG_PORT)
+            logger.info(f"Successfully connected to {db_name}")
+        except psycopg2.OperationalError as e:
+            logger.error(f"Couldn't connect to {db_name}: {e}")
+
+def main(start_height, end_height):
+    ingespy = IndexerDatabase(db_name='indexerdb', user='postgres', password='postgres', host='localhost', port='5432')
+    ingesrs = IndexerDatabase(db_name='indexerdb', user='postgres', password='postgres', host='localhost', port='5432')
+
+    with ingespy.conn as py_conn, ingesrs.conn as rs_conn:
+        with py_conn.cursor() as py_cursor, rs_conn.cursor() as rs_cursor:
+            py_cursor.execute("""
+                                SELECT height, proposer, hash, block_time, txcount, chain_id, total_gas_wanted, total_gas_used, metadata
+                                FROM block_metadata
+                                WHERE height BETWEEN %s AND %s ;
+                                """, (start_height, end_height))
+            py_blocks = py_cursor.fetchall()
+            rs_cursor.execute("""
+                                SELECT height, proposer, hash, block_time, txcount, chain_id, total_gas_wanted, total_gas_used, metadata
+                                FROM block_metadata
+                                WHERE height BETWEEN %s AND %s ;
+                                """, (start_height, end_height))
+            rs_blocks = rs_cursor.fetchall()
+            # TODO: exclude rowid maybe
+            if(py_blocks != rs_blocks):
+                logger.error(f"Inconsistent block data")
+                return False
+
+            py_cursor.execute("""
+                                SELECT height, tx_hash, tx_index, tx_time, code, log, info, gas_wanted, gas_used, fees, codespace
+                                FROM txn_fact_table
+                                WHERE height BETWEEN %s AND %s
+                                ORDER BY height, tx_index;
+                                """, (start_height, end_height))
+            py_txns = py_cursor.fetchall()
+            rs_cursor.execute("""
+                                SELECT height, tx_hash, tx_index, tx_time, code, log, info, gas_wanted, gas_used, fees, codespace
+                                FROM txn_fact_table
+                                WHERE height BETWEEN %s AND %s
+                                ORDER BY height, tx_index;
+                                """, (start_height, end_height))
+            rs_txns = rs_cursor.fetchall()
+            if(py_txns != rs_txns):
+                logger.error(f"Inconsistent transaction data")
+                return False
+            
+            for tx in py_txns:
+                py_cursor.execute("""
+                                    SELECT tx_hash, msg_index, msg_type
+                                    FROM msg_fact_table
+                                    WHERE tx_hash = %s
+                                    ORDER BY msg_index;
+                                    """, (tx[1],))
+                py_msgs = py_cursor.fetchall()
+                rs_cursor.execute("""
+                                    SELECT tx_hash, msg_index, msg_type
+                                    FROM msg_fact_table
+                                    WHERE tx_hash = %s
+                                    ORDER BY msg_index;
+                                    """, (tx[1],))
+                rs_msgs = rs_cursor.fetchall()
+
+                if(py_msgs != rs_msgs):
+                    logger.error(f"Inconsistent transaction message data")
+                    return False
+            
+            return True
+
+if __name__ == "__main__":
+    print(main(19906800, 19906804))
