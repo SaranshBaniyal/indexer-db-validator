7815228a2ba4d41dde911cccf8f3940737c65aa5
Saransh Baniyal <saranshbaniyal14@gmail.com>
2024-05-23 18:16:23 +0530

    added key matching comparision strategy

--- validator-script.py
+++ validator-script.py
@@ -2,6 +2,7 @@ import logging
 import psycopg2
 from psycopg2.extras import RealDictCursor
 
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger()
 
 tx_msg_type = [
@@ -29,15 +30,6 @@ tx_msg_type = [
     'msgcancelunbondingdelegation'
 ]
 
-def compare_dict_lists(a, b, ignore_key=None):
-    if len(a) != len(b):
-        raise ValueError("Lists a and b must have the same length.")
-    
-    for dict_a, dict_b in zip(a, b):
-        for key in dict_a:
-            if key != ignore_key and dict_a.get(key) != dict_b.get(key):
-                return dict_a
-    return None
 
 class IndexerDatabase:
     def __init__(self, db_name, user, password, host, port):
@@ -58,52 +50,86 @@ class IndexerDatabase:
         except psycopg2.OperationalError as e:
             logger.error(f"Couldn't connect to {db_name}: {e}")
 
-def main(start_height, end_height):
-    ingespy = IndexerDatabase(db_name='indexerdb', user='postgres', password='postgres', host='localhost', port='5432')
-    ingesrs = IndexerDatabase(db_name='indexerdb', user='postgres', password='postgres', host='localhost', port='5432')
+def main():
+    ingespy = IndexerDatabase(db_name='indexerdb', user='postgres', password='postgres', host='45.250.253.23', port='5432')
+    ingesrs = IndexerDatabase(db_name='indexerdb', user='postgres', password='postgres', host='45.250.253.23', port='5432')
 
+    logger.info("Starting validator script!")
+
+    #compares data of ingespy against ingesrs
     with ingespy.conn as py_conn, ingesrs.conn as rs_conn:
         with py_conn.cursor(cursor_factory=RealDictCursor) as py_cursor, rs_conn.cursor(cursor_factory=RealDictCursor) as rs_cursor:
             py_cursor.execute("""
                                 SELECT *
                                 FROM block_metadata
-                                WHERE height BETWEEN %s AND %s
                                 ORDER BY height;
-                                """, (start_height, end_height))
+                                """)
             py_blocks = py_cursor.fetchall()
             rs_cursor.execute("""
                                 SELECT *
                                 FROM block_metadata
-                                WHERE height BETWEEN %s AND %s
                                 ORDER BY height;
-                                """, (start_height, end_height))
+                                """)
             rs_blocks = rs_cursor.fetchall()
 
-            result = compare_dict_lists(py_blocks, rs_blocks, "rowid")
-            if result:
-                logger.error(f"Inconsistent block data at height {result['height']}")
-                result=None
-                return False
+            i=0
+            j=0
+            while i<len(py_blocks) and j<len(rs_blocks):
+                if py_blocks[i]['height'] < rs_blocks[j]['height']:
+                    logger.error(f"Couldn't find block of height {py_blocks[i]['height']} in block_metadata table of {ingesrs._PG_DB}")
+                    i+=1
+                elif py_blocks[i]['height'] > rs_blocks[j]['height']:
+                    logger.error(f"Couldn't find block of height {rs_blocks[j]['height']} in block_metadata table of {ingespy._PG_DB}")
+                    j+=1
+                else:
+                    for key in py_blocks[i]:
+                        if key != 'rowid' and py_blocks[i].get(key) != rs_blocks[j].get(key):
+                            logger.error(f"Inconsistent {key} at block height {py_blocks[i]['height']} in block_metadata")
+                    i+=1
+                    j+=1
+
+            while i<len(py_blocks):
+                logger.error(f"Couldn't find block of height {py_blocks[i]['height']} in block_metadata table of {ingesrs._PG_DB}")
+                i+=1
+            while j<len(rs_blocks):
+                logger.error(f"Couldn't find block of height {rs_blocks[j]['height']} in block_metadata table of {ingespy._PG_DB}")
+                j+=1
 
             py_cursor.execute("""
                                 SELECT *
                                 FROM txn_fact_table
-                                WHERE height BETWEEN %s AND %s
                                 ORDER BY height, tx_index;
-                                """, (start_height, end_height))
+                                """)
             py_txns = py_cursor.fetchall()
             rs_cursor.execute("""
                                 SELECT *
                                 FROM txn_fact_table
-                                WHERE height BETWEEN %s AND %s
                                 ORDER BY height, tx_index;
-                                """, (start_height, end_height))
+                                """)
             rs_txns = rs_cursor.fetchall()
-            result = compare_dict_lists(py_txns, rs_txns, "rowid")
-            if result:
-                logger.error(f"Inconsistent transaction data at height {result['height']} and tx_hash {result['tx_hash']}")
-                result = None
-                return False
+
+            i=0
+            j=0
+            while i<len(py_txns) and j<len(rs_txns):
+                if (py_txns[i]['height'] < rs_txns[j]['height']) or (py_txns[i]['height'] == rs_txns[j]['height'] and py_txns[i]['tx_index'] < rs_txns[j]['tx_index']):
+                    logger.error(f"Couldn't find transaction with block height {py_txns[i]['height']} with tx_index {py_txns[i]['tx_index']} in txn_fact_table of {ingesrs._PG_DB}")
+                    i+=1
+                elif (py_txns[i]['height'] > rs_txns[j]['height']) or (py_txns[i]['height'] == rs_txns[j]['height'] and py_txns[i]['tx_index'] > rs_txns[j]['tx_index']):
+                    logger.error(f"Couldn't find transaction with block height {rs_txns[j]['height']} with tx_index {rs_txns[j]['tx_index']} in txn_fact_table of {ingespy._PG_DB}")
+                    j+=1
+                else:
+                    for key in py_txns[i]:
+                        if key != 'rowid' and py_txns[i].get(key) != rs_txns[j].get(key):
+                            logger.error(f"Inconsistent {key} at block height {py_txns[i]['height']} and tx_index {py_txns[j]['tx_index']} in txn_fact_table")
+                    i+=1
+                    j+=1
+            
+            while i<len(py_txns):
+                logger.error(f"Couldn't find transaction with block height {py_txns[i]['height']} and tx_index {py_txns[i]['tx_index']} in txn_fact_table of {ingesrs._PG_DB}")
+                i+=1
+            while j<len(rs_txns):
+                logger.error(f"Couldn't find transaction with block height {rs_txns[j]['height']} and tx_index {rs_txns[j]['tx_index']} in txn_fact_table of {ingespy._PG_DB}")
+                j+=1
             
             for tx in py_txns:
                 py_cursor.execute("""
@@ -121,38 +147,73 @@ def main(start_height, end_height):
                                     """, (tx["tx_hash"],))
                 rs_msgs = rs_cursor.fetchall()
 
-                result = compare_dict_lists(py_msgs, rs_msgs, "rowid")
-                if result:
-                    logger.error(f"Inconsistent transaction message at height {tx['height']}, "
-                                 f"tx_hash {result['tx_hash']}, msg_index {result['msg_index']} and msg_type {result['msg_type']}")
-                    result = None
-                    return False
+                i=0
+                j=0
+                while i<len(py_msgs) and j<len(rs_msgs):
+                    if py_msgs[i]['msg_index'] < rs_msgs[j]['msg_index']:
+                        logger.error(f"Couldn't find message with tx_hash {py_msgs[i]['tx_hash']} and msg_index {py_msgs[i]['msg_index']} in msg_fact_table of {ingesrs._PG_DB}")
+                        i+=1
+                    elif py_msgs[i]['msg_index'] > rs_msgs[j]['msg_index']:
+                        logger.error(f"Couldn't find message with tx_hash {rs_msgs[j]['tx_hash']} and msg_index {rs_msgs[j]['msg_index']} in msg_fact_table of {ingespy._PG_DB}")
+                        j+=1
+                    else:
+                        for key in py_msgs[i]:
+                            if key != 'msg_rowid' and py_msgs[i].get(key) != rs_msgs[j].get(key):
+                                logger.error(f"Inconsistent {key} for tx_hash {py_msgs[i]['tx_hash']} and msg_index{py_msgs[i]['msg_index']} in msg_fact_table")
+                        i+=1
+                        j+=1
+
+                while i<len(py_msgs):
+                    logger.error(f"Couldn't find message with tx_hash {py_msgs[i]['tx_hash']} and msg_index {py_msgs[i]['msg_index']} in msg_fact_table of {ingesrs._PG_DB}")
+                    i+=1
+                while j<len(rs_msgs):
+                    logger.error(f"Couldn't find message with tx_hash {rs_msgs[j]['tx_hash']} and msg_index {rs_msgs[j]['msg_index']} in msg_fact_table of {ingespy._PG_DB}")
+                    j+=1
                 
             for msg_type in tx_msg_type:
                 py_cursor.execute(f"""
                                 SELECT *
                                 FROM {msg_type}
-                                WHERE height BETWEEN {start_height} AND {end_height}
                                 ORDER BY height, hash, index;
-                                """, (start_height, end_height))
+                                """)
                 py_msg_body = py_cursor.fetchall()
                 rs_cursor.execute(f"""
                                     SELECT *
                                     FROM {msg_type}
-                                    WHERE height BETWEEN {start_height} AND {end_height}
                                     ORDER BY height, hash, index;
-                                    """, (start_height, end_height))
+                                    """)
                 rs_msg_body = rs_cursor.fetchall()
 
-                result = compare_dict_lists(py_msg_body, rs_msg_body, "rowid")
-                if result:
-                    logger.error(f"Inconsistent transaction message body at height {tx['height']}, "
-                                 f"tx_hash {result['tx_hash']}, msg_index {result['msg_index']}, msg_type {result['msg_type']} and"
-                                 f"msg_index {result['index']}")
-                    result = None
-                    return False
+                i=0
+                j=0
+                while i<len(py_msg_body) and j<len(rs_msg_body):
+                    if (py_msg_body[i]['height'] < rs_msg_body[j]['height']) or \
+                        (py_msg_body[i]['height'] == rs_msg_body[j]['height'] and py_msg_body[i]['hash'] < rs_msg_body[j]['hash']) or \
+                        (py_msg_body[i]['height'] == rs_msg_body[j]['height'] and py_msg_body[i]['hash'] == rs_msg_body[j]['hash'] and py_msg_body[i]['index'] < rs_msg_body[j]['index']): 
+                        logger.error(f"Couldn't find message entry with block height {py_msg_body[i]['height']}, tx_hash {py_msg_body[i]['hash']} and msg_index {py_msg_body[i]['index']} in {msg_type} table of {ingesrs._PG_DB}")
+                        i+=1
+
+                    elif (py_msg_body[i]['height'] > rs_msg_body[j]['height']) or \
+                        (py_msg_body[i]['height'] == rs_msg_body[j]['height'] and py_msg_body[i]['hash'] > rs_msg_body[j]['hash']) or \
+                        (py_msg_body[i]['height'] == rs_msg_body[j]['height'] and py_msg_body[i]['hash'] == rs_msg_body[j]['hash'] and py_msg_body[i]['index'] > rs_msg_body[j]['index']):
+                        logger.error(f"Couldn't find message entry with block height {rs_msg_body[j]['height']}, tx_hash {rs_msg_body[j]['hash']} and msg_index {rs_msg_body[j]['index']} in {msg_type} table of {ingespy._PG_DB}")
+                        j+=1
+
+                    else:
+                        for key in py_msg_body[i]:
+                            if key != 'rowid' and py_msg_body[i].get(key) != rs_msg_body[j].get(key):
+                                logger.error(f"Inconsistent {key} at block height {py_msg_body[i]['height']}, tx_hash {py_msg_body[i]['hash']} and tx_index {py_txns[j]['index']} in {msg_type} table")
+                        i+=1
+                        j+=1
+
+                while i<len(py_msg_body):
+                    logger.error(f"Couldn't find message entry with block height {py_msg_body[i]['height']}, tx_hash {py_msg_body[i]['hash']} and msg_index {py_msg_body[i]['index']} in {msg_type} table of {ingesrs._PG_DB}")
+                    i+=1
+                while j<len(rs_msg_body):
+                    logger.error(f"Couldn't find message entry with block height {rs_msg_body[j]['height']}, tx_hash {rs_msg_body[j]['hash']} and msg_index {rs_msg_body[j]['index']} in {msg_type} table of {ingespy._PG_DB}")
+                    j+=1
             
-            return True
+            logger.info("Validator script finished!")
 
 if __name__ == "__main__":
-    print(main(19906800, 19906809))
+    main()
